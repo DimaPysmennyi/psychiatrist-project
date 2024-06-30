3605e5b0f81de617d3a49ddd063eb8efb9353765
Servine Survein <gdimon4ik10@gmail.com>
2024-06-30 11:46:11 +0300

    updated header, contacts, about. added phone support

--- clinic/clinicapp/static/clinicapp/js/header.js
+++ clinic/clinicapp/static/clinicapp/js/header.js
@@ -1,37 +1,7 @@
-const inst = document.querySelector("#inst");
-const facebook = document.querySelector("#facebook");
+const checkBtn = document.querySelector(".checkbtn");
+const check =  document.querySelector("#check");
 
-
-
-inst.addEventListener("mouseover", (event) => {
-    inst.style.color = "#faf5f5";
-    inst.style.backgroundColor = "#4e574b";
-    inst.style.cursor = "pointer";
-});
-
-inst.addEventListener("mouseout", (event) => {
-    inst.style.backgroundColor = "#faf5f5";
-    inst.style.color = "#4e574b";
-});
-
-inst.addEventListener("click", (event) => {
-    window.location.replace("https://www.instagram.com/yaroslava_timchenko?igsh=MTNiOXR2cndtejQ3cA==");
+checkBtn.addEventListener("click", (event) => {
+    check.checked = true;
 })
-
-facebook.addEventListener("mouseover", (event) => {
-    facebook.style.color = "#faf5f5";
-    facebook.style.backgroundColor = "#4e574b";
-    facebook.style.cursor = "pointer";
-});
-
-facebook.addEventListener("mouseout", (event) => {
-    facebook.style.backgroundColor = "#faf5f5";
-    facebook.style.color = "#4e574b";
-});
-
-facebook.addEventListener("click", (event) => {
-    window.location.replace("https://www.facebook.com/Yaroslava.Tymchenko5");
-})
-
-
 
